bb8f8aec50f22f69193de18b54bc11740fa776e1
ElRacoon <lea.granier@outlook.fr>
2025-01-15 20:05:26 +0100

    add command for running serve and change port

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const factRoutes = require('./routes/factRoutes');
-
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 3001;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 
-app.use('/api', factRoutes);
+app.get('/api/anecdotes', (req, res) => {
+    res.json([
+        { id: 1, text: "Anecdote 1" },
+        { id: 2, text: "Anecdote 2" }
+    ]);
+});
 
+// Lancer le serveur
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`API running on http://localhost:${PORT}`);
 });
