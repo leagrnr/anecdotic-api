59a67b38fcc3b8568d0cbf0f9464a467a65418cd
ElRacoon <lea.granier@outlook.fr>
2025-01-19 16:45:59 +0100

    index with factsRoute

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.get('/api/anecdotes', (req, res) => {
-    res.json([
-        { id: 1, text: "Anecdote 1" },
-        { id: 2, text: "Anecdote 2" }
-    ]);
+app.use('/facts', require('./routes/factsRoutes'));
+
+app.get('/', (req, res) => {
+    res.send('hello world');
 });
 
-// Lancer le serveur
 app.listen(PORT, () => {
     console.log(`API running on http://localhost:${PORT}`);
 });
